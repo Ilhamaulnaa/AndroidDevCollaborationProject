1761a2be571606b8dee755a898bd05c843e625f0
Ilham <Ilhamjkt@gmail.com>
2024-02-23 15:25:44 +0700

    Add feature-login

---
+++ app/src/main/java/com/android/androiddevcollaboration/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.android.androiddevcollaboration.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.androiddevcollaboration.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
